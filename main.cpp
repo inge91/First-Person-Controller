6a66d8a6a91b6a2078982a55ee506604f5379e4f
Inge Becht <inge.becht91@gmail.com>
2013-10-23 23:37:23 +0200

    I wanted to give an elaborate description of the changes but I do not understand how git diff works. This is a very sad day indeed

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@ using namespace std;
 int width = 800;
 int height = 600;
 
-int rotatex = 0;
+int rotatex = 180;
 int rotatey = 0;
+
+double forward = 0;
 
 void keyboard(unsigned char c, int x, int y)
 {
+    if(c == 'w' )
+    {
+        forward += 0.5;
+    
+    }
 
 }
 
@@ -20,15 +27,25 @@ void display()
     glMatrixMode(GL_MODELVIEW );
     glLoadIdentity();
 
-    glRotated(rotatex, 0, 1, 0);
     glRotated(rotatey, 1, 0, 0);
-    cout<<"rotatex: "<<rotatex<<endl;
+    glTranslated(0, 0, forward);
+    glRotated(rotatex, 0, 1, 0);
+    
 
     glBegin(GL_TRIANGLES);
-    glVertex3f(-1, 0 , -2);
+    glVertex3f(-1, 0, -2);
     glVertex3f(1, 0, -2);
     glVertex3f(0, 1, -2);
     glEnd();
+
+    glBegin(GL_QUADS);
+    glVertex3f(-4,-4, -4);
+    glVertex3f(4, -4, -4);
+    glVertex3f(4, -4, 4);
+    glVertex3f(-4, -4, 4);
+    glEnd();
+
+
 
 
     glutSwapBuffers();
@@ -39,19 +56,28 @@ void reshape(int w, int h)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport( 0, 0, w, h);
-    gluPerspective(90, w/h, 1, 100);
-
+    gluPerspective(90, w/h, 0, 100);
 }
 
 void update_orientation(int x, int y)
 {
     // Mouse changed in x or y direction;
-   rotatex += ((x - width/2))/2  ;
+   rotatex += ((x - width/2))/4  ;
    rotatex = rotatex % 360;
-   rotatey += ((y - height/2))/2  ;
-   rotatey = rotatey % 360;
-        
-    glutWarpPointer(width/2, height/2);
+
+   
+   // In case of y
+   rotatey += ((y - height/2))/4  ;
+   if(rotatey > 50){
+       rotatey = 50;
+   }
+   if(rotatey < -50)
+   {
+       rotatey = -50;
+   }
+
+   glutWarpPointer(width/2, height/2);
+   glFlush();
 }
 
 
@@ -62,6 +88,8 @@ int main(int argc, char **argv){
 
     glutInitWindowSize(width,height);
     glutCreateWindow("First person Controller Test");
+
+    glutSetCursor(GLUT_CURSOR_NONE);
     
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
